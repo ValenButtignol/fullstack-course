2ce4756dcca87c6f11011f6a9b7b90030f6f03ba
ValenButtignol <valentinbuttignol@gmail.com>
2025-10-02 17:58:28 -0300

    Exercise 2.8, and fix alert when submitting now

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,31 +3,58 @@ import Persons from './components/Persons';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleSubmitForm = (event) => {
     event.preventDefault();
-    const newPhoneContact = {name: newName};
+    if (newNumber === '') {
+      alert("Number can't be empty, try again")
+      return;
+    }
+
+    if (repeatedPerson(newName)) {
+      alert(`${newName} is already used, try again`)
+      return;
+    }
+
+    if (repeatedNumber(newNumber)) {
+      alert(`${newNumber} is already used, try again`)
+      return;
+    }  
+    
+    const newPhoneContact = {
+      name: newName,
+      number: newNumber 
+    };
     setPersons(persons.concat(newPhoneContact));
     setNewName('');
+    setNewNumber('');
   }
 
   const repeatedPerson = (name) => {
     return persons.some(person => person.name === name);
   }
 
+  const repeatedNumber = (number) => {
+    return persons.some(person => person.number === number);
+  }
+
+  const handleNewNumberButton = (event) => {
+    const number = event.target.value;
+    setNewNumber(number);
+  }
+
   const handleNewNameButton = (event) => {
     const name = event.target.value;
-    if (repeatedPerson(name)) {
-      alert(`${name} is already added to phonebook`)
-      setNewName('')
-      return;
-    }
-
     setNewName(name);
   }
+  
 
 
   return (
@@ -36,6 +63,9 @@ const App = () => {
       <form onSubmit={handleSubmitForm}>
         <div>
           name: <input value={newName} onChange={handleNewNameButton}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNewNumberButton}/>
         </div>
         <div>
           <button type="submit">add</button>
