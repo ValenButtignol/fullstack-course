27fdc2c4203e19d57fb0fdafef5fe18dab6efe39
ValenButtignol <valentinbuttignol@gmail.com>
2025-10-02 00:59:57 -0300

    Exercise 2.1 and 2.2

---
+++ part2/course-app/src/components/Course.jsx
@@ -0,0 +1,42 @@
+const Course = ({course}) => {
+  const Header = ({title}) => {
+    return (
+      <>
+        <h1>{title}</h1>
+      </>
+    )
+  }
+
+  const Content = ({ parts }) => {
+    const content = parts.map(part => <Part key={part.name} part={part} />);
+    return (
+      <>
+        {content}
+      </>
+    );
+  }
+
+  const Part = ({ part }) => {
+    return (
+      <>
+        <p> {part.name} {part.exercises}</p>
+      </>
+    )
+  }
+
+  const Total = ({parts}) => {
+    let total = 0;
+    parts.forEach( part => {total += part.exercises} );
+    return <p>Number of exercises {total}</p>
+  }
+
+  return (
+    <div>
+      <Header title={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
+    </div>
+  )
+}
+
+export default Course;
